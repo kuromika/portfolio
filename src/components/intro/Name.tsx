257b4660df87779b00a09230963a0fe5e2a3227c
mika-alpha <yuukeikioku@gmail.com>
2023-02-07 10:28:19 -0500

    Remove intermittenting name from Intro

--- src/components/intro/Name.tsx
+++ src/components/intro/Name.tsx
@@ -6,30 +6,10 @@ type LinkedName = null | {
 };
 
 export const Name = () => {
-  const [name, setName] = useState("Jean Alvarez");
-
-  useEffect(() => {
-    const first: LinkedName = { name: "Jean Alvarez", next: null };
-
-    const second: LinkedName = { name: "Kuromika", next: first };
-
-    first.next = second;
-
-    let current: LinkedName = first;
-
-    const interval = setInterval(() => {
-      if (current) {
-        setName(current.name);
-        current = current.next;
-      }
-    }, 2000);
-
-    return () => clearInterval(interval);
-  }, []);
 
   return (
     <h1 className="text-center text-2xl font-bold uppercase sm:text-left sm:text-3xl md:text-4xl lg:text-5xl">
-      <span aria-hidden="true" aria-label="Jean Alvarez">{name}</span>
+      <span>Jean Alvarez</span>
       <span className="mt-3 block sm:ml-20">full-stack developer</span>
     </h1>
   );
